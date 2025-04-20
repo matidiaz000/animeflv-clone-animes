6e4bf9d825295e21f0f229244a2e60d7ab42eadc
Matias Díaz <matidiaz00@gmail.com>
2025-04-20 18:51:53 -0300

    initial project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import Animes from './pages/Animes';
+
+const App = () => {
+  return (
+    <Animes />
+  );
+};
+
+export default App;
---
+++ src/pages/Animes.tsx
@@ -0,0 +1,9 @@
+const Animes = () => {
+  return (
+    <>
+      <h1>Animes page microfrontend</h1>
+    </>
+  );
+};
+
+export default Animes;
