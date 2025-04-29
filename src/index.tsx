d4a447ddc51c8c1778500d34c823bb6509a057b4
Matias Díaz <matidiaz00@gmail.com>
2025-04-29 12:52:09 -0300

    consuming anilist api

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,19 @@ import { BrowserRouter } from "react-router-dom";
 import App from './App';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import React from 'react';
+import { relayStylePagination } from "@apollo/client/utilities";
 
 const client = new ApolloClient({
   uri: 'https://graphql.anilist.co/',
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          Page: relayStylePagination(),
+        } 
+      }
+    }
+  }),
 });
 
 const container = document.getElementById("root") as HTMLElement;
--- src/pages/Animes.tsx
+++ src/pages/Animes.tsx
@@ -2,26 +2,21 @@ import { Button, Card } from "@matidiaz000/animeflv-clone-library";
 import { useParams } from "react-router-dom";
 import { useQuery } from "@apollo/client";
 import { GET_ANIME_LIST } from "../queries/animes.list";
-import { getSeason } from "../utilities/seasons";
+import { currentYear, getSeason } from "../utilities/seasons";
+import { useState } from "react";
+import { urls } from "../constant/urls";
+import { Format } from "../constant/format";
 
 const Animes = () => {
   const { category } = useParams();
-
-  const currentYear = new Date().getFullYear();
-
-  const urls: { url: string, sort: string }[] = [
-    { url: 'episodios-nuevos', sort: 'ID_DESC' },
-    { url: 'lo-mas-popular', sort: 'POPULARITY_DESC' },
-    { url: 'proximamente', sort: 'POPULARITY_DESC' },
-    { url: 'top-100-anime', sort: 'SCORE_DESC' },
-  ]
+  const [currentPage, setCurrentPage] = useState(1);
+  const getSort = urls.find(d => d.url === category)?.sort;
 
   const setVariables = () => {
-    const sort = urls.find(d => d.url === category)?.sort;
     let variables: any = {
-      sort: [sort],
-      page: 1,
-      perPage: 20,
+      sort: [getSort],
+      page: currentPage,
+      perPage: 18,
     }
     if (category === urls[1].url) {
       variables = {
@@ -40,32 +35,37 @@ const Animes = () => {
     return variables
   }
 
-  const list = useQuery(GET_ANIME_LIST, {
+  const { loading, error, data, fetchMore } = useQuery(GET_ANIME_LIST, {
     fetchPolicy: 'cache-and-network',
     variables: setVariables(),
   });
+  
+  const media = data?.Page?.media;
+  const pageInfo = data?.Page?.pageInfo;
 
-  const Format = (string: any) => {
-    if (string === 'MOVIE') return 'film'
-    if (string === 'TV') return 'anime'
-    if (string === 'OVA') return 'ova'
-    else return 'default'
-  }
+  const loadMore = (page: number) => {
+    if (pageInfo.hasNextPage) {
+      setCurrentPage(page)
+      fetchMore({ variables: setVariables() });
+    }
+  };
 
-  if (list.loading) return <p>Loading...</p>;
-  if (list.error) return <p>Error : {list.error.message}</p>;
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
 
   return (
     <article className="container pb-5">
       <header className="d-flex justify-content-between align-items-center mt-5">
-        <h2>Acción</h2>
+        <h2>{urls.find(d => d.url === category)?.text}</h2>
+        {/* 
         <div className="d-flex align-items-center">
           <Button variant="contained" className="ms-3 rounded-pill" color="primary" startIcon="">Ordenar</Button>
           <Button variant="contained" className="ms-3 rounded-pill" color="primary" startIcon="">Filtrar</Button>
         </div>
+        */}
       </header>
       <section className="row my-n4 py-4">
-        {list?.data?.Page?.media?.map((item: any) => 
+        {media?.map((item: any) => 
           <div className="col-2 my-4" key={`animesList-${item.id}`}>
             <Card
               img={item.coverImage?.large}
@@ -76,6 +76,22 @@ const Animes = () => {
             />
           </div>
           )}
+      </section>
+      <section className="d-flex">
+        <Button
+          disabled={currentPage === 1}
+          variant="contained"
+          className="mx-auto mt-4"
+          onClick={() => loadMore(currentPage - 1)}
+          color="primary"
+        >Anterior</Button>
+        <span>{currentPage} / {pageInfo?.lastPage}</span>
+        <Button
+          variant="contained"
+          className="mx-auto mt-4"
+          onClick={() => loadMore(currentPage + 1)}
+          color="primary"
+        >Siguiente</Button>
       </section>
     </article>
   );
